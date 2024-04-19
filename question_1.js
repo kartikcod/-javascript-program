aefcac282b445ceda3577be1e7adcedb4a1d7b3b
kartik mahato <118644047+kartikcod@users.noreply.github.com>
2024-04-19 21:28:17 +0530

    Add files via upload

---
+++ question_1.js
@@ -0,0 +1,20 @@
+//Create a const object called “product” to store information shown in the picture.
+
+ const product={
+    name:"pen",
+    rating:"4",
+    type:"jail pen",
+    price:"270",
+    discount:"5%",
+    color:"black white"
+ }
+ console.log(product)
+
+ const profile={
+    name:"shardha khapra",
+    total_post:"195",
+    follower:"569k",
+    following:"4",
+    blue_tic:"enable",
+}
+console.log(profile);
---
+++ question_2.js
@@ -0,0 +1,14 @@
+//Qs2. Write a code which can give grades to students according to their scores:
+
+const number = prompt("enter the mark")
+if (number <= 80) {
+    console.log("a")
+} else if (number <= 70) {
+    console.log("b")
+} else if (number <= 60) {
+    console.log("c")
+} else if (number <= 50) {
+    console.log("d")
+} else {
+    console.log("f")
+}
